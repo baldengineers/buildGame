273ac1d71ad7263fcfbc8549fe5148dfedb5bcbf
Jony <liu.z.jonathan@gmail.com>
2017-12-03 16:32:36 -0800

    Complete rewrite
    I completely rewrote the graphics, and now multiple people joining the
    game and moving around is working

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ server.listen(process.env.PORT || 1266, function() {
 	console.log('Server running on port %s', port);
 });
 
+var players = []; //list of all the players on the server
+
 io.on('connection', function(socket) {
+	var currentPlayer; //stores the player being referenced by socket
 	console.log('user connected');
 	
-	socket.on('playerData', function(data) {
-		//console.log('playerData', data);
+	socket.on('movePlayer', function(data) {
+		//console.log('playerData', data.x);
 		//calculate player movement
 		if (!(data.dir.up && data.dir.down)) {
 			if (data.dir.up) {
@@ -32,10 +35,47 @@ io.on('connection', function(socket) {
 			}
 		}
 		
+		//console.log(data.x);
+		//OBSELETE -  -- - - - - -- - - --MAJOR FLAW WITH THIS, it sets it back to original after moving, unless the physical player is moving
+		players.forEach(function(player, index, players) {
+			if (player.id == data.id) {
+				players[index].x = data.x;
+				players[index].y = data.y;
+				//console.log('setting index ' + index + ' x pos to ' + data.x);
+			}
+		});
+		
 		socket.emit('movePlayer', {x: data.x, y: data.y});
+		//socket.broadcast.emit('playerData', {id: data.id, x: data.x, y: data.y});
+	});
+	
+	socket.on('sync', function() {
+		//sync the game with the server
+		socket.emit('playerData', players);
 	});
 	
 	socket.on('joinGame', function(player) {
-		socket.broadcast.emit('addPlayer', player);
+		currentPlayer = player;
+		
+		//send the list of current players to the new player
+		socket.emit('addPlayers', players); 
+		
+		//send new player to everybody else
+		socket.broadcast.emit('addPlayers', [player]);
+	
+		//ad new player to the list
+		players.push(player);
+		console.log(players);
+	});
+	
+	socket.on('disconnect', function() {
+		/*for (var i = players.length-1; i >= 0; i--) {
+			if (players[i].id == socketID) {
+				players.splice(i, 1);
+			}
+		}*/
+		var i = players.indexOf(currentPlayer);
+		players.splice(i, 1);
+		console.log(currentPlayer.id + ' left.');
 	});
 });
---
+++ www/js/main2.js
@@ -0,0 +1,168 @@
+function GameArea(width, height, socket) {
+	this.width = width;
+	this.height = height;
+	this.canvas = document.createElement('canvas');
+	this.players = [];
+	this.currentPlayer;
+	this.socket = socket;
+}
+
+GameArea.prototype = { 
+	start: function() {
+		this.canvas.width = this.width;
+		this.canvas.height = this.height;
+		this.context = this.canvas.getContext('2d');
+		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		this.setControls();
+		
+		var t = this;
+		setInterval(function() {
+			t.updateGameArea();
+		}, 20);
+	},
+	
+	clear: function() {
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	},
+	
+	updateGameArea: function() {
+		//console.log('players UPDATEGA', this.players);
+		
+		this.sendCurPlayerData();
+		this.sync();
+		this.clear();
+		
+		this.players.forEach(function(p) {
+			p.update();
+		});
+		if (this.currentPlayer != undefined) {
+			this.currentPlayer.update();
+		}
+	},
+	
+	addPlayer: function(x, y, id, isCurrent) {
+		console.log('players before: ', this.players);
+		var p = new Player(this.context, x, y, id, isCurrent);
+		if (p.isCurrent) {
+			this.currentPlayer = p;
+			console.log('currentplayer', this.currentPlayer);
+		} else {
+			this.players.push(p);
+			console.log('add player with id of: ' + id);
+		}
+		console.log('players after: ', this.players);
+	},
+	
+	sync: function() {
+		this.socket.emit('sync');
+	},
+	
+	sendCurPlayerData: function() {
+		if (this.currentPlayer != undefined) {
+			var data = this.currentPlayer.getData();
+			//console.log(data);
+			this.socket.emit('movePlayer', data);
+		}
+	},
+	
+	receivePlayerData: function(data) {
+		//receive positions of all the players. Data is [{id: id, x: x, y: y}]
+		//console.log(this.players);
+		
+		data.forEach(function(item) {
+			//console.log('item id ', item.id);
+			console.log(this.players);
+			if (this.players != undefined) {
+				this.players.forEach(function(player) {
+					//console.log('player id ', player.id);
+					if (item.id == player.id) {
+						player.moveTo(item.x, item.y);
+					}
+				});
+			}
+		}, this); //without this "this" all the this. variables are undefined!
+	},
+	
+	setControls: function() {
+		var t = this;
+		$(document).keydown(function(e) {
+			var k = e.keyCode || e.which;
+			switch(k) {
+				case 87:
+					//w
+					t.currentPlayer.dir.up = true;
+					break;
+				case 68:
+					//d
+					t.currentPlayer.dir.right = true;
+					break;
+				case 83:
+					//s
+					t.currentPlayer.dir.down = true;
+					break;
+				case 65:
+					//a
+					t.currentPlayer.dir.left = true;
+					break;
+			}
+		}).keyup(function(e) {
+			var k = e.keyCode || e.which;
+			switch(k) {
+				case 87:
+					//w
+					t.currentPlayer.dir.up = false;
+					break;
+				case 68:
+					//d
+					t.currentPlayer.dir.right = false;
+					break;
+				case 83:
+					//s
+					t.currentPlayer.dir.down = false;
+					break;
+				case 65:
+					//a
+					t.currentPlayer.dir.left = false;
+					break;
+			}
+		});
+		
+		$(window).blur(function() {
+			t.currentPlayer.dir = {up: false, right: false, down: false, left: false};
+		});
+	}
+}
+
+function Player(context, x, y, id, isCurrent) {
+	this.x = x;
+	this.y = y;
+	this.color = 'red';
+	this.ctx = context;
+	this.id = id;
+	this.isCurrent = isCurrent; //is it the current player
+	this.dir = {up: false, right: false, down: false, left: false};
+	this.speed = 5;
+	this.init();
+}
+
+Player.prototype = {
+	init: function() {
+		this.update();
+	},
+	
+	update: function() {
+		this.ctx.fillStyle = this.color;
+		this.ctx.fillRect(this.x, this.y, 30, 30);
+	},
+	
+	getData: function() {
+		//console.log('dir', this.dir);
+		return {x: this.x, y: this.y, dir: this.dir, speed: this.speed, id: this.id};
+	},
+	
+	moveTo: function(x, y) {
+		this.x = x;
+		this.y = y;
+		//console.log('move to %d, %d', x, y);
+	},
+}
